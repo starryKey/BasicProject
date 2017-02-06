d1ff254688b8c8ebe714d4bc76a34140b27112e8
starryKey <dahua@lijinlindemac-mini.local>
2017-02-06 14:26:29 +0800

    Initial Commit

---
+++ BasicProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicProject
+//
+//  Created by Li_JinLin on 17/2/6.
+//  Copyright © 2017年 www.dahuatech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BasicProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BasicProject
+//
+//  Created by Li_JinLin on 17/2/6.
+//  Copyright © 2017年 www.dahuatech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
